f6c20a925f639498faf6aaab7bb48149938de8b7
isengrims <104489572+isengrims@users.noreply.github.com>
2023-02-10 12:18:40 +0400

    allow to retrieve gRPC service from handler context (#21)
    * allow to retrieve gRPC service from handler context
    * support JSR-310 date and time types in settings

--- src/main/kotlin/com/exactpro/th2/conn/dirty/tcp/core/Main.kt
+++ src/main/kotlin/com/exactpro/th2/conn/dirty/tcp/core/Main.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2021 Exactpro (Exactpro Systems Limited)
+ * Copyright 2021-2023 Exactpro (Exactpro Systems Limited)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@ import com.exactpro.th2.conn.dirty.tcp.core.api.impl.DummyManglerFactory
 import com.exactpro.th2.conn.dirty.tcp.core.util.load
 import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinFeature.NullIsSameAsDefault
 import com.fasterxml.jackson.module.kotlin.KotlinModule.Builder
 import mu.KotlinLogging
@@ -73,6 +74,7 @@ fun main(args: Array<String>) = try {
 
     val mapper = JsonMapper.builder()
         .addModule(Builder().configure(NullIsSameAsDefault, true).build())
+        .addModule(JavaTimeModule())
         .addModule(module)
         .build()
 
@@ -87,7 +89,8 @@ fun main(args: Array<String>) = try {
         eventRouter,
         messageRouter,
         handlerFactory,
-        manglerFactory
+        manglerFactory,
+        factory.grpcRouter
     ) { resource, destructor ->
         resources += resource to destructor
     }.run()
